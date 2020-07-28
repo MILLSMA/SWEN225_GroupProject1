e2486c8a3ac2f5d6848c095832cd26fabc96ecde
Johniel Bocacao <50508869+johnielb@users.noreply.github.com>
2020-07-28 14:37:29 +1200

    Enum made in Card class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,11 @@
-public class Card {
+interface Card {
+
+}
+
+enum WeaponCard implements Card {
+    WEAPON_ONE,
+    WEAPON_TWO,
+    WEAPON_THREE;
+
+    public WeaponCard deal() { return null; }
 }
--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,5 @@
  * Main class that uses that holds all of the game mechanics.
  */
 public class Game {
+    WeaponCard card = WeaponCard.WEAPON_ONE;
 }
