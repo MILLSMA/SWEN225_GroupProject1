a342e3ed1c8b590c87d0db2be15acf68f0d0fcab
MILLSMA <millsmafb@gmail.com>
2020-08-29 17:39:47 +1200

    Junit setup (hopefully)

---
+++ tests/tests.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static  org.junit.Assert.*;
+public class tests {
+
+    /**
+     * just ensuring Junit is correctly set up
+     */
+    @Test
+    public void sampleTest(){
+        int num = 6;
+        assert (num == 6);
+
+    }
+
+}
